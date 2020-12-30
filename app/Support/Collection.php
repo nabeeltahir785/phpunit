d9989f01e636888444f7f700680ca4b555c06a3d
nabeelmehmood789 <nabeel@rebeltechnology.io>
2020-12-30 13:08:56 +0500

    PHpunit

--- app/Support/Collection.php
+++ app/Support/Collection.php
@@ -3,8 +3,10 @@
 
 namespace App\Support;
 
+use IteratorAggregate;
+use JsonSerializable;
 
-class Collection
+class Collection implements IteratorAggregate, JsonSerializable
 {
     protected  $items = [];
 
@@ -18,6 +20,31 @@ class Collection
 
     public function count(){
         return count($this->items);
+    }
+
+    public function getIterator()
+    {
+        // TODO: Implement getIterator() method.
+        return new \ArrayIterator($this->items);
+    }
+
+
+    public function add(array $items){
+        $this->items = array_merge($this->items,$items);
+    }
+    public function merge(Collection $collection){
+        return $this->add($collection->get());
+    }
+
+    public function toJson(){
+        return json_encode($this->items);
+    }
+
+    public function jsonSerialize()
+    {
+        // TODO: Implement jsonSerialize() method.
+
+        return $this->items;
     }
 
 
--- tests/unit/CollectionTest.php
+++ tests/unit/CollectionTest.php
@@ -38,4 +38,84 @@ class CollectionTest extends  TestCase
         $this->assertEquals('Three',$collection->get()[2]);
     }
 
+    /** @test */
+
+    public function collection_is_instance_of_iterator_aggregate(){
+        $collection = new \App\Support\Collection();
+        $this->assertInstanceOf(\IteratorAggregate::class,$collection);
+    }
+
+    /** @test */
+
+    public function collection_can_be_iterated(){
+        $collection = new \App\Support\Collection([
+            'One','Two','Three'
+        ]);
+
+        $items = [];
+
+        foreach($collection as $item){
+            $items[] = $item;
+        }
+
+        $this->assertCount(3,$items);
+        $this->assertInstanceOf(\ArrayIterator::class,$collection->getIterator());
+
+    }
+
+    /** @test */
+
+    public function collection_can_be_merged_with_another_collection(){
+        $collection1 = new \App\Support\Collection([
+            'One','Two','Three'
+        ]);
+        $collection2 = new \App\Support\Collection([
+            'Four','Five','Six'
+        ]);
+
+        $collection1->merge($collection2);
+
+        $this->assertCount(6,$collection1->get());
+        $this->assertEquals(6,$collection1->count());
+    }
+    /** @test */
+
+    public function can_add_to_existing_collection(){
+        $collection = new \App\Support\Collection([
+            'One','Two','Three'
+        ]);
+
+        $collection->add(['three']);
+
+        $this->assertEquals(4,$collection->count());
+        $this->assertCount(4,$collection->get());
+    }
+
+    /** @test */
+
+    public function return_json_encoded_items(){
+        $collection = new \App\Support\Collection([
+            ["fname"=>"M. Nabeel Mehmood"],
+            ["lname"=>"Tahir Mehmood"]
+        ]);
+
+        $this->assertEquals('[{"fname":"M. Nabeel Mehmood"},{"lname":"Tahir Mehmood"}]',$collection->toJson());
+        $this->assertIsString($collection->toJson());
+    }
+
+
+    /** @test */
+
+    public function json_encoding_a_collection_object_returns_json(){
+        $collection = new \App\Support\Collection([
+            ["fname"=>"M. Nabeel Mehmood"],
+            ["lname"=>"Tahir Mehmood"]
+        ]);
+
+        $encoded = json_encode($collection);
+        $this->assertEquals('[{"fname":"M. Nabeel Mehmood"},{"lname":"Tahir Mehmood"}]',$encoded);
+        $this->assertIsString($encoded);
+    }
+
+
 }
